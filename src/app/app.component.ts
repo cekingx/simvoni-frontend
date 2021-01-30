bd62867c68a4afadc5514891a9b16a72e29c3590
Dirga Yasa <cekingx@gmail.com>
2021-01-30 20:02:54 +0800

    smooth loading animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { LoadingService } from "./services/loading.service";
+import { Subscription } from "rxjs";
+import { 
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+ } from "@angular/animations";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('popOverState', [
+      state('show', style({
+        opacity: 1,
+      })),
+      state('hide', style({
+        opacity: 0,
+        zIndex: -10
+      })),
+      transition('show => hide', animate('500ms ease')),
+      transition('hide => show', animate('500ms ease'))
+    ])
+  ]
 })
 export class AppComponent {
   title = 'Index Simvoni';
+  loadingEventSubscription: Subscription;
+  showLoading = false;
+
+  constructor(private loadingService: LoadingService)
+  {
+    this.loadingEventSubscription = this.loadingService
+    .getLoadingEvent()
+    .subscribe(state => {
+      this.showLoading = state;
+    });
+  }
+
+  get stateName() 
+  {
+    return this.showLoading ? 'show' : 'hide';
+  }
+
 }
