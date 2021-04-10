ec31adff081ae73806175a053620e475a354ef5f
Dirga Yasa <cekingx@gmail.com>
2021-04-10 23:34:30 +0800

    unsubscribe

--- src/app/super-admin/pages/show-ea/show-ea.component.ts
+++ src/app/super-admin/pages/show-ea/show-ea.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { LoadingService } from '@app/services/loading.service';
 import { ElectionAuthority } from '@app/_models/ea';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'sv-super-admin-show-ea',
   templateUrl: './show-ea.component.html',
   styleUrls: ['./show-ea.component.scss']
 })
-export class ShowEaComponent implements OnInit {
+export class ShowEaComponent implements OnInit, OnDestroy {
+  subscription1$: Subscription;
   electionAuthority: ElectionAuthority;
   eaId: number;
 
@@ -18,9 +20,13 @@ export class ShowEaComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.route.data.subscribe((data: any) => {
+    this.subscription1$ = this.route.data.subscribe((data: any) => {
       this.electionAuthority = data.electionAuthority;
       this.loadingService.hideLoading();
     });
+  }
+
+  ngOnDestroy() {
+    this.subscription1$.unsubscribe();
   }
 }
