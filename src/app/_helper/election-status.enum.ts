1996dc095c7bdbb1743e2fd586da55a725d48b9d
Dirga Yasa <cekingx@gmail.com>
2021-08-27 12:59:03 +0800

    enum for ElectionStatus

---
+++ src/app/_helper/election-status.enum.ts
@@ -0,0 +1,8 @@
+export enum ElectionStatus {
+  draft = 'draft',
+  ready_to_deploy = 'ready_to_deploy',
+  deployed = 'deployed',
+  started = 'started',
+  ended = 'ended',
+  rejected = 'rejected'
+}
