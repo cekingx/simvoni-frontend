e6c3be83febca2e8888d79f35e1de9a7c110a035
Dirga Yasa <cekingx@gmail.com>
2022-07-26 15:30:13 +0800

    feat(weight): ui create weight

---
+++ src/app/_models/create-weight.dto.ts
@@ -0,0 +1,4 @@
+export class CreateWeightDto {
+  name: string;
+  weight: number;
+}
--- src/app/election-authority/pages/create-weight/create-weight.component.ts
+++ src/app/election-authority/pages/create-weight/create-weight.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ElectionService } from '@app/election-authority/services/election.service';
+import { LoadingService } from '@app/services/loading.service';
+import { BreadcrumbItem } from '@app/_models/breadcrumb-item';
+import { CreateWeightDto } from '@app/_models/create-weight.dto';
 
 @Component({
   selector: 'app-create-weight',
@@ -6,10 +12,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-weight.component.scss']
 })
 export class CreateWeightComponent implements OnInit {
+  breadcrumbItems: BreadcrumbItem[] = [
+    {
+      name: 'Dashboard',
+      route: '/election-authority'
+    }
+  ]
+  createWeightForm: FormGroup;
+  submitted = false;
+  error = '';
 
-  constructor() { }
+  constructor(
+    private loadingService: LoadingService,
+    private electionService: ElectionService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm()
+  {
+    this.createWeightForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      weight: new FormControl('', [Validators.required])
+    });
+  }
+
+  get formControl()
+  {
+    return this.createWeightForm.controls;
+  }
+
+  onSubmit()
+  {
+    this.submitted = true;
+    let createWeightDto: CreateWeightDto;
+
+    if( this.createWeightForm.invalid) {
+      return;
+    }
+
+    createWeightDto = {
+      name: this.formControl.name.value,
+      weight: this.formControl.weight.value
+    }
+    this.loadingService.showLoading();
+    // this.electionService.createWeight(createWeightDto)
+    //   .subscribe(
+    //     result => {
+    //       this.loadingService.hideLoading();
+    //     },
+    //     err => {
+    //       this.error = err.error.message;
+    //       this.loadingService.hideLoading();
+    //     }
+    //   );
+  }
+
+  redirectBack()
+  {
+    this.router.navigate(['election-authority/election']);
   }
 
 }
